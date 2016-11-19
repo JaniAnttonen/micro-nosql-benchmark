2d986b19c0cfb753d1e068e25fbbd963887dd7c7
Jani Anttonen <jantto@utu.fi>
2016-11-19 23:34:49 +0200

    add redis test

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const { send } = require('micro')
 const sleep = require('then-sleep')
 const crypto = require('crypto')
-const redis = require('connect-redis')
-const mongo = require('connect-mongo')
+const redis = require('redis').createClient()
+const mongo = require('mongodb').MongoClient
+const perfy = require('perfy')
+
+const createCipher = (password) => {
+  return crypto.createCipher('aes192', `${password}${Date.now()}`)
+}
+
+const test = (iterations) => {
+  let i = 0
+  for (i; i < iterations; i++) {
+    redis.set(`${i}`, `${createCipher("Ebin")}`);
+  }
+}
 
 module.exports = async (req, res) => {
-  await sleep(500)
-  send(res, 200, 'Ready!')
+  perfy.start('timeNeeded')
+  await test(1000000)
+  send(res, 200, `The test took ${perfy.end('timeNeeded').time} s`)
 }
