9b195fd3b8f9be104a6b3020305d86852bbebe35
Jani Anttonen <jantto@utu.fi>
2016-12-13 00:36:46 +0200

    add a static file to show the tests

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const { send } = require('micro')
+const url = require('url')
+const qs = require('querystring')
+const nodeStatic = require('node-static')
 const crypto = require('crypto')
 
 // The tests
@@ -10,6 +13,7 @@ const createCipher = (password) => {
   return crypto.createCipher('aes192', `${password}${Date.now()}`)
 }
 
+// Create an array of dummy session data with a specified length
 const createData = (iterations, cipher) => {
   const items = []
   let i = 0
@@ -19,11 +23,23 @@ const createData = (iterations, cipher) => {
   return items
 }
 
+// File server for the test html page
+const fileServer = new nodeStatic.Server()
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serveFile('/index.html', 200, {}, request, response)
+    }).resume()
+}).listen(8080)
+
+/**
+ * Endpoints for the performance tests
+ */
 module.exports = async (req, res) => {
   // Create dummy session hash
   const cipher = createCipher('Ebin')
   const data = createData(100, cipher)
 
+  console.log(req)
   const redisTime = await testRedis(data)
   const mongoTime = await testMongo(data)
   
