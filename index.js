96f5c021cddff1e4a8f7728d199030b6221b7762
Jani Anttonen <jantto@utu.fi>
2019-09-30 14:09:40 +0300

    remove unnecessary ciphertext

--- index.js
+++ index.js
@@ -1,97 +1,93 @@
-const {
-  send
-} = require('micro')
-const http = require('http')
-const url = require('url')
-const qs = require('querystring')
-const serveStatic = require('serve-static')
-const finalhandler = require('finalhandler')
-const crypto = require('crypto')
+const { send } = require("micro");
+const http = require("http");
+const url = require("url");
+const qs = require("querystring");
+const serveStatic = require("serve-static");
+const finalhandler = require("finalhandler");
+const crypto = require("crypto");
 
 // The tests
-const writeRedis = require('./redis').writeRedis
-const readRedis = require('./redis').readRedis
-const writeMongo = require('./mongo').writeMongo
-const readMongo = require('./mongo').readMongo
+const writeRedis = require("./redis").writeRedis;
+const readRedis = require("./redis").readRedis;
+const writeMongo = require("./mongo").writeMongo;
+const readMongo = require("./mongo").readMongo;
 
 // Function for creating dummy session data
-const createHash = (password) => {
-  return crypto.createCipher('aes192', `${password}${Date.now()}`)
-}
+const createHash = password => {
+  return `${password}${Date.now()}`;
+};
 
 const userData = () => ({
   isAdmin: false,
   language: "13375P34K"
-})
+});
 
 // Create an array of dummy session data with a specified length
 const createData = (iterations, hash) => {
-  const items = []
-  let i = 0
+  const items = [];
+  let i = 0;
   for (i; i < iterations; i++) {
-    items.push([i, hash, userData])
+    items.push([i, hash, userData]);
   }
-  return items
-}
+  return items;
+};
 
-let prevData = []
+let prevData = [];
 
 /**
  * File server for the html test page
  */
-const serve = serveStatic('public/', {
-  'index': 'index.html'
-})
+const serve = serveStatic("public/", {
+  index: "index.html"
+});
 
-// Create server 
+// Create server
 var staticServer = http.createServer(function onRequest(req, res) {
-  serve(req, res, finalhandler(req, res))
-})
+  serve(req, res, finalhandler(req, res));
+});
 
-// Listen 
-staticServer.listen(8000)
-
+// Listen
+staticServer.listen(8000);
 
 /**
  * Endpoints for the performance tests
  */
-module.exports = async(req, res) => {
+module.exports = async (req, res) => {
   // Parse request
-  const endpoint = url.parse(req.url).pathname
-  const query = qs.parse(url.parse(req.url).query)
-  const iterations = query && query.iter ? query.iter : 100
-  const user = query && query.user ? query.user : 'Ebin'
+  const endpoint = url.parse(req.url).pathname;
+  const query = qs.parse(url.parse(req.url).query);
+  const iterations = query && query.iter ? query.iter : 100;
+  const user = query && query.user ? query.user : "Ebin";
 
   // Create dummy session hash
-  const hash = createHash(user)
-  const data = createData(iterations, hash)
+  const hash = createHash(user);
+  const data = createData(iterations, hash);
 
   // Push new data to memory
-  data.filter((index, hash, userData) => prevData.push(hash))
+  data.filter((index, hash, userData) => prevData.push(hash));
 
   // Default time
-  let elapsed = 0
+  let elapsed = 0;
 
   // Routing
   switch (endpoint) {
-    case '/rediswrite':
-    elapsed = await writeRedis(data)
-    case '/mongowrite':
-    elapsed = await writeMongo(data)
-    case '/redisread':
-    elapsed = await readRedis(
-      prevData.slice(prevData.length - iterations, prevData.length - 1)
-      )
-    case '/mongoread':
-    elapsed = await readMongo(
-      prevData.slice(prevData.length - iterations, prevData.length - 1)
-      )
+    case "/rediswrite":
+      elapsed = await writeRedis(data);
+    case "/mongowrite":
+      elapsed = await writeMongo(data);
+    case "/redisread":
+      elapsed = await readRedis(
+        prevData.slice(prevData.length - iterations, prevData.length - 1)
+      );
+    case "/mongoread":
+      elapsed = await readMongo(
+        prevData.slice(prevData.length - iterations, prevData.length - 1)
+      );
   }
 
-
   // Allow the test site to control the API
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Send data
-  send(res, 200, `${elapsed}`)
-}
+  send(res, 200, `${elapsed}`);
+};
